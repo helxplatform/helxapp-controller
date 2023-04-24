57716308a23d0bea970a291f5dfe510e0baacde5
waTeim <truthset@gmail.com>
2023-04-23 22:20:40 -0400

    Initial commit

---
+++ pkg/controller/helxapp_controller.go
@@ -0,0 +1,54 @@
+package controller
+
+import (
+	"context"
+
+	"github.com/go-logr/logr"
+	"github.com/helxplatform/helxapp-controller/pkg/helxapp"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/kubernetes"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+type HeLxAppReconciler struct {
+	client.Client
+	clientset *kubernetes.Clientset
+	Log       logr.Logger
+	Scheme    *runtime.Scheme
+}
+
+func NewHeLxAppReconciler(mgr ctrl.Manager, clientset *kubernetes.Clientset) *HeLxAppReconciler {
+	return &HeLxAppReconciler{
+		Client:    mgr.GetClient(),
+		clientset: clientset,
+		Log:       ctrl.Log.WithName("controllers").WithName("HeLxApp"),
+		Scheme:    mgr.GetScheme(),
+	}
+}
+
+func (r *HeLxAppReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&helxapp.HeLxApp}). // Replace with the actual type of your custom resource.
+		Complete(r)
+}
+func (r *HeLxAppReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	log := r.Log.WithValues("helxapp", req.NamespacedName)
+
+	// Fetch the HeLxApp instance
+	instance := &helxapp.HeLxApp{}
+	err := r.Get(ctx, req.NamespacedName, instance)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			log.Info("HeLxApp not found. Ignoring since object must be deleted.")
+			return ctrl.Result{}, nil
+		}
+		// Error reading the object - requeue the request.
+		return ctrl.Result{}, err
+	}
+
+	// Your reconciliation logic here...
+
+	return ctrl.Result{}, nil
+}
---
+++ pkg/helxapp/helxapp_types.go
@@ -0,0 +1,45 @@
+package helxapp
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// Service represents a service to be created within the HeLxApp
+type Service struct {
+	Name        string            `json:"name"`
+	Labels      map[string]string `json:"labels,omitempty"`
+	Annotations map[string]string `json:"annotations,omitempty"`
+}
+
+// HeLxAppSpec defines the desired state of HeLxApp
+type HeLxAppSpec struct {
+	Replicas int32     `json:"replicas"`
+	Services []Service `json:"services"`
+	Version  string    `json:"version"`
+}
+
+// HeLxAppStatus defines the observed state of HeLxApp
+type HeLxAppStatus struct {
+	// Add your fields here, e.g., current service configurations.
+}
+
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+
+// HeLxApp is the Schema for the helxapps API
+type HeLxApp struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   HeLxAppSpec   `json:"spec,omitempty"`
+	Status HeLxAppStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// HeLxAppList contains a list of HeLxApp
+type HeLxAppList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []HeLxApp `json:"items"`
+}
